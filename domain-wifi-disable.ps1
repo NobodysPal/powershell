b7b03296f7a61175c046666e63dbed7349f87f56
NobodysPal <nobodyspal@netscape.net>
2016-04-20 21:28:11 -0500

    Added files via upload

---
+++ domain-wifi-disable.ps1
@@ -0,0 +1,48 @@
+﻿function domain-wifi-disable{             
+    [CmdletBinding()]             
+    param (             
+        [parameter(
+            Position=0,            
+            Mandatory=$true,            
+            ValueFromPipeline=$true,             
+            ValueFromPipelineByPropertyName=$true)
+        ]            
+    [string]$computer=".",
+    [string]$domain="WORKGROUP" #set theis to your domain or workgroup to check for             
+    )
+                 
+    BEGIN{
+        $ErrorActionPreference = "Stop"
+        [int]$OSVersion = [environment]::OSVersion.Version | select -ExpandProperty Major
+    }#begin             
+
+    PROCESS{            
+        Try{
+            $getdomain = Get-WmiObject -Class Win32_ComputerSystem -ComputerName $computer | select -ExpandProperty Domain -ErrorAction Stop
+            $wificard = Get-WmiObject -Class Win32_NetworkAdapter -filter "Name LIKE '%Wireless%'" -ErrorAction Stop
+        }
+        Catch [Exception]{
+            write-host “Caught an exception:” -ForegroundColor Red
+            write-host “Exception Type: $($_.Exception.GetType().FullName)” -ForegroundColor Red
+            write-host “Exception Message: $($_.Exception.Message)” -ForegroundColor Red
+            Break
+        }
+           
+    }#process             
+
+    END{
+        if($getdomain -eq $domain -and $wificard){
+            if($OSVersion -le 6){
+                $wificard.disable()
+            }
+            else{
+                Get-NetAdapter -Name Wi-Fi | Disable-NetAdapter -Confirm $false
+            }
+            return $true
+        }
+        else{
+            return $false
+        }
+    }#end            
+            
+}
