89ee4031c6a8e3527811027a6f74a5b2ccd208aa
NobodysPal <nobodyspal@netscape.net>
2016-04-24 23:04:11 -0500

    So many changes...

--- collect-files.ps1
+++ collect-files.ps1
@@ -1,4 +1,4 @@
-﻿function collect-files{             
+﻿function Collect-Files{             
     [CmdletBinding()]             
     param (             
         [parameter(
@@ -7,11 +7,65 @@
             ValueFromPipeline=$true,             
             ValueFromPipelineByPropertyName=$true)
         ]            
-    [string]$path=".",
-    [switch]$recursive             
+        [string]$path=".",
+        [switch]$recursive=$false,
+        [string]$outfile="C:\Users\Jeff\Desktop\files.txt"     
     )
                  
     BEGIN{
+        Clear-Host
+        function Error-Collection($error){
+            write-Host “[!] Caught an exception” -ForegroundColor Red
+            write-Host “[!] Exception Type: $($_.Exception.GetType().FullName)” -ForegroundColor Red
+            write-Host “[!] Exception Message: $($_.Exception.Message)” -ForegroundColor Red
+            Break
+        }
+
+        function Hash-Files{
+            Try{
+                $files = Get-Content $outfile
+                foreach($file in $files){
+                    $progress = $files.count - $files.IndexOf($file)
+                    if((Get-Item $file).length -gt 10mb){
+                        Write-Host "[!] File is larget than 10mb, skipping for performance:" $file -ForegroundColor Yellow    
+                    }
+                    else{
+                        Get-FileHash -Algorithm MD5 $file -ErrorAction SilentlyContinue | Out-Null
+                    }
+                    Write-Progress -Activity “Hashing all the files. $progress remaining” -status “Hashing file: $file” -percentComplete ($files.IndexOf($file) / $files.count*100)
+                }
+            }
+            Catch [Exception]{
+                Error-Collection($_.Exception)
+            }
+            Write-Progress -Activity “Hashing all the files” -status “All Done” -PercentComplete 100
+            Start-Sleep -Seconds 3
+        }
+
+        function User-Prompt{
+            param(
+                [Parameter(Mandatory=$true)]
+                [string]$title = "",
+                [Parameter(Mandatory=$true)]
+                [string]$message = "",
+                [Parameter(Mandatory=$true)]
+                $yesaction,
+                [Parameter(Mandatory=$true)]
+                $noaction
+            )
+
+            $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Agrees to the action or question."
+            $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", "Does not agree with the action."
+            $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
+            $result = $host.ui.PromptForChoice($title, $message, $options, 0) 
+
+            switch($result)
+                {
+                    0 { Invoke-Expression $yesaction }
+                    1 { Invoke-Expression $noaction }
+                }
+        }
+
         Try{
             Write-Host "[!] Testing for availability of: "$path -ForegroundColor Yellow
             if(-not (Test-Path -Path $path)){
@@ -26,15 +80,13 @@
             }
         }
         Catch [Exception]{
-            write-Host “[!] Caught an exception” -ForegroundColor Red
-            write-Host “[!] Exception Type: $($_.Exception.GetType().FullName)” -ForegroundColor Red
-            write-Host “[!] Exception Message: $($_.Exception.Message)” -ForegroundColor Red
-            Break
+            Error-Collection($_.Exception)
         }
     }#begin             
 
     PROCESS{            
         Try{
+            User-Prompt -title "Ready to Execute File Collection?" -message "This can cause a strain on the system if a large directory is selected." -yesaction Continue -noaction Exit
             if($recursive){
                 $files = Get-ChildItem -Path $path -Recurse -File -ErrorAction SilentlyContinue | Select -ExpandProperty FullName
             }
@@ -43,29 +95,21 @@
             }
         }
         Catch [Exception]{
-            write-Host “[!] Caught an exception” -ForegroundColor Red
-            write-Host “[!] Exception Type: $($_.Exception.GetType().FullName)” -ForegroundColor Red
-            write-Host “[!] Exception Message: $($_.Exception.Message)” -ForegroundColor Red
-            Break
+            Error-Collection($_.Exception)
         }
            
     }#process             
 
     END{
-            Write-Host "[!]" $files.Count "files to Hash" -ForegroundColor Yellow
-            foreach ($file in $files) {
-                $position = $files.Count - $files.IndexOf($file)
-                Write-Progress -Activity “Hashing all the files. $position Left” -status “Hashing: $file” -percentComplete ($files.IndexOf($file) / $files.count*100)
-                if((Get-Item $file).length -lt 10mb){
-                    Get-FileHash -Algorithm MD5 $file -ErrorAction SilentlyContinue | Out-Null
-                }
-                else{
-                    Write-Host "[!] File is greater than 10MB, skipping for performance:" $file -ForegroundColor Yellow
-                }
-            }
-            Write-Progress -Activity “Hashing all the files.” -status “Complete” -PercentComplete 100
-            Start-Sleep -Seconds 3
-            Write-Host "[#] All Done." -ForegroundColor Green
+        Try{
+            Out-File -InputObject $files -FilePath $outfile
+            Write-Host "[#] All done collecting files. Output written to file: $outfile" -ForegroundColor Green
+            User-Prompt -title "Hash the files?" -message "Would you like to hash the files now?" -yesaction Hash-Files -noaction 'Write-Host "[#] Response was No." -ForegroundColor Green'
+            Write-Host "[#] Fin...Seriously..." -ForegroundColor Green
+        }
+        Catch [Exception]{
+            Error-Collection($_.Exception)
+        }
     }#end            
             
 }
