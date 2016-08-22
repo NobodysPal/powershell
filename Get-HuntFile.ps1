6fcfeeb7f62469c31d83c0c68ce8d74b66dfea5b
NobodysPal <nobodyspal@netscape.net>
2016-08-21 19:04:22 -0500

    Updates

--- Get-HuntFile.ps1
+++ Get-HuntFile.ps1
@@ -67,18 +67,40 @@ Function Get-HuntFile {
         Extension = $Null
         }
     $output = $NUll
+    $ascii = "IAAgACAAXwBfAF8AXwBfACAAIAAgACAAIAAgAF8AIAAgACAAIAAgACAAIAAgACAAIABfACAAIAAg
+              ACAAXwAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgAF8AIAAgACAAXwBfAF8AXwBfAF8AIABfACAA
+              XwAgACAAIAAgACAAIAAKACAAIAAvACAAXwBfAF8AXwB8ACAAIAAgACAAfAAgAHwAIAAgACAAIAAg
+              ACAAIAAgAHwAIAB8ACAAIAB8ACAAfAAgACAAIAAgACAAIAAgACAAIAAgACAAfAAgAHwAIAB8ACAA
+              IABfAF8AXwBfACgAXwApACAAfAAgACAAIAAgACAACgAgAHwAIAB8ACAAIABfAF8AIAAgAF8AXwBf
+              AHwAIAB8AF8AIABfAF8AXwBfAF8AXwB8ACAAfABfAF8AfAAgAHwAXwAgACAAIABfACAAXwAgAF8A
+              XwAgAHwAIAB8AF8AfAAgAHwAXwBfACAAIAAgAF8AfAAgAHwAIABfAF8AXwAgAAoAIAB8ACAAfAAg
+              AHwAXwAgAHwALwAgAF8AIABcACAAXwBfAHwAXwBfAF8AXwBfAF8AfAAgACAAXwBfACAAIAB8ACAA
+              fAAgAHwAIAB8ACAAJwBfACAAXAB8ACAAXwBfAHwAIAAgAF8AXwB8ACAAfAAgAHwAIAB8AC8AIABf
+              ACAAXAAKACAAfAAgAHwAXwBfAHwAIAB8ACAAIABfAF8ALwAgAHwAXwAgACAAIAAgACAAIAAgAHwA
+              IAB8ACAAIAB8ACAAfAAgAHwAXwB8ACAAfAAgAHwAIAB8ACAAfAAgAHwAXwB8ACAAfAAgACAAIAAg
+              AHwAIAB8ACAAfAAgACAAXwBfAC8ACgAgACAAXABfAF8AXwBfAF8AfABcAF8AXwBfAHwAXABfAF8A
+              fAAgACAAIAAgACAAIAB8AF8AfAAgACAAfABfAHwAXABfAF8ALABfAHwAXwB8ACAAfABfAHwAXABf
+              AF8AfABfAHwAIAAgACAAIAB8AF8AfABfAHwAXABfAF8AXwB8AAoAIAAgACAAIAAgACAAIAAgACAA
+              IAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAg
+              ACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAA="
     
+    # Writes Header information
+    $header = [Text.Encoding]::Unicode.GetString([Convert]::FromBase64String($ascii))
+    Write-Host $header -ForegroundColor DarkGray
+    Write-Host "  Author: Jeff Pierdomenico  Version: 1.0BETA  Date: 13 Aug 2016 `n" -ForegroundColor DarkGray
+
     # Sets up the output directory
-    $outputpath = $(Split-Path -Path $PSCommandPath) + "\output"
+    $outputpath = "C:\Users\Administrator\Desktop\output\" #$(Split-Path -Path $PSCommandPath) + "\output"
+    $outputfile = "HuntFile-" + $((get-date).ToUniversalTime()).ToString("yyyyMMddTHHmmZ")
     Try {    
         If (Test-Path -Path $outputpath){
             Write-Verbose "[i] Output path okay."
-            $outputpath = $outputpath + "\Get-HuntFiles.xml"
+            $outputpath = $outputpath + $outputfile
         }
         Else {
             New-Item -ItemType directory -Path $outputpath
             Write-Warning "[!] New directory created at $outputpath"
-            $outputpath = $outputpath + "\Get-HuntFiles.xml"
+            $outputpath = $outputpath + $outputfile
         }
     }
     Catch {
@@ -89,11 +111,10 @@ Function Get-HuntFile {
     Function Drive-Cleanup {
         Write-Verbose "[i] Validating Drive: $drive"
         If ($drive.Length -gt 2) {
-            Write-Verbose "In IF"
             $drive = $drive.Substring(0,2)
         }
         If ($drive.EndsWith(":") -ne $true) {
-        $drive = $drive.Substring(0,1) + ":"
+            $drive = $drive.Substring(0,1) + ":"
         }
         $file.Drive = $drive
     }
@@ -115,41 +136,57 @@ Function Get-HuntFile {
     # Combine parameters into WMI filter
     Function Build-Filter {
         Write-Verbose "[i] Assembling filter."
-        $filter = "filename='$($file.Name)' AND extension='$($file.Extension)' AND Drive='$drive'"
+        if ($file.Name -eq '*') {
+            $filter = "extension='$($file.Extension)' AND Drive='$drive'"
+        }
+        else {
+            $filter = "filename='$($file.Name)' AND extension='$($file.Extension)' AND Drive='$drive'"
+        }
         Add-Member -InputObject $file –MemberType NoteProperty –Name "Filter" –value $filter
     }
 
     # Execute the main functions
     Try {
-
         Drive-Cleanup        
         File-Cleanup
         Build-Filter
         Write-Verbose "[i] Starting Jobs on $computers..."
         if ($credential) {
             $credentials = Get-Credential -Credential "DOMAIN\USER"
-            $job = Get-WmiObject -Class CIM_Datafile -Filter $file.Filter -ComputerName $computers -Credential $credentials -AsJob
+            foreach ($computer in $computers) { 
+                Get-WmiObject -Class CIM_Datafile -Filter $file.Filter -ComputerName $computer -AsJob | Format-Table
+            }
+            $jobs = Get-Job
         }
         else {
-            $job = Get-WmiObject -Class CIM_Datafile -Filter $file.Filter -ComputerName $computers -AsJob
-        }
-        while ( $job.State -eq 'Running') {
-            for ($I = 1; $I -le 100; $I++ ) {
-                Write-Progress -Activity "Job in Progress" -PercentComplete $I
-                Start-Sleep -Milliseconds 10
+            foreach ($computer in $computers) { 
+                Get-WmiObject -Class CIM_Datafile -Filter $file.Filter -ComputerName $computer -AsJob | Format-Table
             }
+            $jobs = Get-Job
         }
-        Write-Progress -Activity "Done!" -PercentComplete 100
+        
+        $totaljobs = (Get-Job).Count
+        While ((Get-Job).State -match 'Running')
+        {
+            foreach ($job in  $jobs | where {$_.HasMoreData})
+            {
+                #Write-Progress -Activity “Running Jobs” -status “Systems Remaining: $(($jobs | where {$_.HasMoreData}).Count) ” -percentComplete $((($jobs | where {$_.HasMoreData}).Count) / $totaljobs*100)
+                Write-Progress -Activity “Running Jobs” -status “Systems Remaining: $(((Get-Job).State -match 'Running').Count) ” -percentComplete $((((Get-Job).State -match 'Running').Count) / $totaljobs*100)
+
+            }
+        Start-Sleep -Milliseconds 100
+        } 
+        Write-Progress -Activity "Done!" -PercentComplete 0
         Start-Sleep -Seconds 1
         Write-Verbose "[i] Recieving data from job..."
         
         # If the fulloutput switch is passed on the command line, all properties for the found files will be displayed.
         # Else there will be an abreviated output of Host Name and FIle Location
         if ($fulloutput) {
-            $output = Receive-Job -Id $job.Id | Select-Object -Property *
+            $output = Receive-Job -Id $jobs.Id | Select-Object -Property *
         }
         else {
-            $output = Receive-Job -Id $job.Id | Select-Object -Property @{
+            $output = Receive-Job -Id $jobs.Id | Select-Object -Property @{
                     Name="Host Name"
                     Expression={$_.PSComputername}
                 }, 
--- Get-WMIFileHash.psm1
+++ Get-WMIFileHash.psm1
@@ -1,5 +1,44 @@
-﻿Function Get-WMIFileHash {
-# Assumes domain credentials
+﻿<#
+.SYNOPSIS
+  Hashing files using WMI only
+
+.DESCRIPTION
+  Hashing files using WMI only
+
+.TODO
+  (1) Async
+
+  
+.NOTES
+  Version:        1.0
+  Author:         Jeff Pierdomenico
+  Creation Date:  21 August 2016
+  Purpose/Change: Initial script development
+
+#>
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+
+#Set Default Error Action
+$ErrorActionPreference = 'Stop'
+
+#Import PSLogging Module
+#Import-Module PSLogging
+
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+
+#Script Version
+$sScriptVersion = '1.0'
+
+#Log File Info
+#$sLogPath = 'C:\Windows\Temp'
+#$sLogName = '<script_name>.log'
+#$sLogFile = Join-Path -Path $sLogPath -ChildPath $sLogName
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+Function Get-WMIFileHash {
+#comment based help is here
 
     [cmdletbinding()]
     Param(
@@ -12,27 +51,85 @@
         [string]$dohash="%TEMP%\do-hash.ps1"
     )
     
+    # Sets the verbosity
     $VerbosePreference = "Continue"
 
+    $ascii = "DQAKACAAIAAgAF8AXwBfAF8AXwAgACAAIAAgACAAIABfACAAIAAgACAAIAAgACAAXwBfACAAIAAg
+              ACAAIAAgACAAIAAgACAAXwBfAF8AXwAgACAAXwBfACAAXwBfAF8AXwBfACAAXwBfAF8AXwBfAF8A
+              IABfACAAXwAgACAAIAAgACAAIABfACAAIAAgACAAXwAgACAAIAAgACAAIAAgACAAIAAgACAAXwAg
+              ACAAIAAgACAADQAKACAAIAAvACAAXwBfAF8AXwB8ACAAIAAgACAAfAAgAHwAIAAgACAAIAAgACAA
+              XAAgAFwAIAAgACAAIAAgACAAIAAgAC8AIAAvACAAIABcAC8AIAAgAHwAXwAgACAAIABfAHwAIAAg
+              AF8AXwBfAF8AKABfACkAIAB8ACAAIAAgACAAfAAgAHwAIAAgAHwAIAB8ACAAIAAgACAAIAAgACAA
+              IAAgAHwAIAB8ACAAIAAgACAADQAKACAAfAAgAHwAIAAgAF8AXwAgACAAXwBfAF8AfAAgAHwAXwAg
+              AF8AXwBfAF8AXwBcACAAXAAgACAALwBcACAAIAAvACAALwB8ACAAXAAgACAALwAgAHwAIAB8ACAA
+              fAAgAHwAIAB8AF8AXwAgACAAIABfAHwAIAB8ACAAXwBfAF8AfAAgAHwAXwBfAHwAIAB8ACAAXwBf
+              ACAAXwAgAF8AXwBfAHwAIAB8AF8AXwAgACAADQAKACAAfAAgAHwAIAB8AF8AIAB8AC8AIABfACAA
+              XAAgAF8AXwB8AF8AXwBfAF8AXwBfAFwAIABcAC8AIAAgAFwALwAgAC8AIAB8ACAAfABcAC8AfAAg
+              AHwAIAB8ACAAfAAgAHwAIAAgAF8AXwB8ACAAfAAgAHwAIAB8AC8AIABfACAAXAAgACAAXwBfACAA
+              IAB8AC8AIABfACAALwAgAF8AXwB8ACAAJwBfACAAXAAgAA0ACgAgAHwAIAB8AF8AXwB8ACAAfAAg
+              ACAAXwBfAC8AIAB8AF8AIAAgACAAIAAgACAAIAAgAFwAIAAgAC8AXAAgACAALwAgACAAfAAgAHwA
+              IAAgAHwAIAB8AF8AfAAgAHwAXwB8ACAAfAAgACAAIAAgAHwAIAB8ACAAfAAgACAAXwBfAC8AIAB8
+              ACAAIAB8ACAAfAAgACgAXwB8ACAAXABfAF8AIABcACAAfAAgAHwAIAB8AA0ACgAgACAAXABfAF8A
+              XwBfAF8AfABcAF8AXwBfAHwAXABfAF8AfAAgACAAIAAgACAAIAAgACAAXAAvACAAIABcAC8AIAAg
+              ACAAfABfAHwAIAAgAHwAXwB8AF8AXwBfAF8AXwB8AF8AfAAgACAAIAAgAHwAXwB8AF8AfABcAF8A
+              XwBfAHwAXwB8ACAAIAB8AF8AfABcAF8AXwAsAF8AfABfAF8AXwAvAF8AfAAgAHwAXwB8AA0ACgAg
+              ACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAA
+              IAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAg
+              ACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAA
+              IAAgAA0ACgAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAg
+              ACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAA
+              IAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAg
+              ACAAIAAgACAAIAAgAA=="
+    
+    # Writes Header information
+    $header = [Text.Encoding]::Unicode.GetString([Convert]::FromBase64String($ascii))
+    Write-Host $header -ForegroundColor DarkGray
+    Write-Host "  Author: Jeff Pierdomenico  Version: 1.0BETA  Date: 21 Aug 2016 `n" -ForegroundColor DarkGray
+
+    # Creating the empty hashed file object
+    $hashedfile = New-Object -TypeName PsObject -Property @{
+        Time = $Null
+        System = $Null
+        File = $Null
+        Hash = $Null
+        }
+
+    # Builds the encoded command to pass to the remote system
+    $command = {
+        New-EventLog -LogName HASHLOG -Source HASHLOG
+        $hash=Invoke-Expression "certutil.exe -hashfile <file>" -ErrorAction SilentlyContinue
+        $result=$hash[1] | Out-String
+        if($result -match "cannot"){
+            Write-EventLog -LogName HASHLOG -Source HASHLOG -EventId 0834 -EntryType Information -Message "$(Get-Date -Format u)|$($env:computername)|<file>|Failed"
+        }
+        else{Write-EventLog -LogName HASHLOG -Source HASHLOG -EventId 0834 -EntryType Information -Message "$(Get-Date -Format u)|$($env:computername)|<file>|$($result.Replace(' ' ,''))"
+        }
+    }
+    $command = $command -replace "<file>",$hashfile
+    $encodedCommand = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($command))
+
+    # Combines all the arguments required for the command line
+    $argumentlist = " -NoLogo -NoProfile -Noninteractive -ExecutionPolicy Unrestricted -EncodedCommand"
+    
+    # Clean up commands
+    $cleanup = {
+        Remove-EventLog -LogName HASHLOG
+        Set-ExecutionPolicy Default -Force
+    }
+    $cleanup =[Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($cleanup))
+    
     Try {
         foreach ($computer in $computers) {     
-            Write-Verbose "[$computer] Creating a new event log named 'HASHLOG'"
-            Invoke-WmiMethod -ComputerName $computer -Class Win32_Process -Name Create -ArgumentList "cmd.exe /c powershell.exe New-EventLog -LogName HASHLOG -Source HASHLOG" | Out-Null
-            Write-Verbose "[$computer] Writitng the file to execute."
-            Invoke-WmiMethod -ComputerName $computer -Class Win32_Process -Name Create -ArgumentList "cmd.exe /c echo `$file=`"$hashfile`" >> $dohash" | Out-Null
-            Invoke-WmiMethod -ComputerName $computer -Class Win32_Process -Name Create -ArgumentList "cmd.exe /c echo `$hash=Invoke-Expression `"certutil.exe `-hashfile `$file`" -ErrorAction SilentlyContinue >> $dohash" | Out-Null
-            Invoke-WmiMethod -ComputerName $computer -Class Win32_Process -Name Create -ArgumentList "cmd.exe /c echo `$result=`$hash[1] ^| Out-String >> $dohash" | Out-Null
-            Invoke-WmiMethod -ComputerName $computer -Class Win32_Process -Name Create -ArgumentList "cmd.exe /c echo if(`$result -match `"cannot`"){Write-EventLog -LogName HASHLOG -Source HASHLOG -EventId 0834 -EntryType Information -Message `"`$(Get-Date -Format u)|`$(`$env:computername)|`$(`$file)|Failed`"} >> $dohash" | Out-Null
-            Invoke-WmiMethod -ComputerName $computer -Class Win32_Process -Name Create -ArgumentList "cmd.exe /c echo else{Write-EventLog -LogName HASHLOG -Source HASHLOG -EventId 0834 -EntryType Information -Message `"`$(Get-Date -Format u)|`$(`$env:computername)|`$(`$file)|`$(`$result.Replace(' ' ,''))`"} >> $dohash" | Out-Null
-            Start-Sleep -Seconds 1
-            Write-Verbose "[$computer] Setting the Exectuion Policy to 'Unrestricted'"
-            Invoke-WmiMethod -ComputerName $computer -Class Win32_Process -Name Create -ArgumentList "cmd.exe /c powershell.exe Set-ExecutionPolicy Unrestricted -Force" | Out-Null
-            Start-Sleep -Seconds 1
-            Write-Verbose "[$computer] Running the file 'do-hash.ps1'"
-            Invoke-WmiMethod -ComputerName $computer -Class Win32_Process -Name Create -ArgumentList "cmd.exe /c powershell.exe $dohash" | Out-Null
-            Start-Sleep -Seconds 1
+            # Tasking the remote system with the encoded commands
+            Write-Verbose "[$computer] Sending the tasking script encoded"
+            Write-Verbose $encodedCommand
+            Invoke-WmiMethod -ComputerName $computer -Class Win32_Process -Name Create -ArgumentList "cmd.exe /c powershell.exe $argumentlist $encodedCommand" | Out-Null
+
+            # Pausing for a few seconds to let the remote system hash the file
+            Start-Sleep -Seconds 5
+
+            # TIme to retrieve the hash
             Write-Verbose "[$computer] Retrieving the file hash..."
-            #Start-Sleep -Seconds 5
             [string]$message = Get-WmiObject -ComputerName $computer -query "SELECT * FROM Win32_NTLogEvent WHERE (logfile='HASHLOG' and SourceName='HASHLOG')" | Select-Object -ExpandProperty Message
             $i=1
             While(($message.Length -eq 0) -and ($i -lt 10)) {
@@ -41,18 +138,20 @@
                 $i++
                 Start-Sleep -Seconds 10
             }
-            Write-Host $message -ForegroundColor Yellow
+            
+            # Assemble the file object
+            $hashedfile.Time = $($message.Split("|")[0]).Trim()
+            $hashedfile.System = $($message.Split("|")[1]).Trim()
+            $hashedfile.File = $($message.Split("|")[2]).Trim()
+            $hashedfile.Hash = $($message.Split("|")[3]).Trim()
+            $hashedfile | Format-List System,File,Hash,Time
         }
     }
     Finally {
         Write-Verbose "[i] Cleaning up..."
-        Invoke-WmiMethod -ComputerName $computers -Class Win32_Process -Name Create -ArgumentList "cmd.exe /c del /F $dohash" | Out-Null
+        Write-Verbose $cleanup
+        Invoke-WmiMethod -ComputerName $computers -Class Win32_Process -Name Create -ArgumentList "cmd.exe /c powershell.exe $argumentlist $cleanup" | Out-Null
         Start-Sleep -Seconds 1
-        Write-Verbose "[i] File 'do-hash.ps1' removed from target system"
-        Write-Verbose "[i] Retunring Execution Policy to 'Default'"
-        Invoke-WmiMethod -ComputerName $computers -Class Win32_Process -Name Create -ArgumentList "cmd.exe /c powershell.exe Set-ExecutionPolicy Default -Force" | Out-Null
-        Start-Sleep -Seconds 1
-        Invoke-WmiMethod -ComputerName $computers -Class Win32_Process -Name Create -ArgumentList "cmd.exe /c powershell.exe Remove-EventLog HASHLOG" | Out-Null
         Write-Verbose "[i] Done"    
     }
 }
